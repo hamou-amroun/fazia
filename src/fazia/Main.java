97d1b0e375a7e9fd91cf04a05dec72f9a2ec795c
Hamou AMROUN <hamou.amroun@gmail.com>
2021-06-01 01:43:36 +0200

    3 ed commit

--- src/fazia/Main.java
+++ src/fazia/Main.java
@@ -3,6 +3,6 @@ package fazia;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Heiii fzrogik ");
+        System.out.println("Hello world");
     }
 }
